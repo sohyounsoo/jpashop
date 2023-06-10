4197c74ef9f905c4133d45802d84841f0f739459
So hyounsoo <so5663@naver.com>
2023-06-10 15:16:58 +0900

    member service

--- src/test/java/jpabook/jpashop/service/MemberServiceTest.java
+++ src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -1,25 +1,24 @@
-package jpabook.jpashop;
+package jpabook.jpashop.service;
 
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.repository.MemberRepository;
-import jpabook.jpashop.service.MemberService;
 import org.junit.Assert;
-import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.junit.runner.RunWith;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
-import javax.transaction.Transactional;
 
-import static org.junit.Assert.fail;
-
+import static org.junit.jupiter.api.Assertions.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@Transactional() //테스트 후 롤백을 한다 단 test에서만
-public class MemberRepositoryTest {
+@Transactional
+public class MemberServiceTest {
 
     @Autowired MemberService memberService;
     @Autowired MemberRepository memberRepository;
@@ -30,29 +29,21 @@ public class MemberRepositoryTest {
         //given
         Member member = new Member();
         member.setName("kim");
-        
+
         //when
         Long saveId = memberService.join(member);
 
         //then
-        em.flush();
-        Assert.assertEquals(member, memberRepository.findOne(saveId));
+        assertEquals(member, memberRepository.findOne(saveId));
     }
 
-    @Test(expected = IllegalStateException.class)
+    @Test
     public void 중복_회원_예외() throws Exception {
         //given
-        Member member1 = new Member();
-        member1.setName("kim");
-
-        Member member2 = new Member();
-        member2.setName("kim");
 
         //when
-        memberService.join(member1);
-        memberService.join(member2); // 예외가 발생해야 한다.
 
         //then
-        fail("예외가 발생해야 한다.");
     }
+
 }
